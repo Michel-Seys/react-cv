8eeb2fbe5114e01698d2e7eb0225992bc5dca5c6
Michel Seys <seysmich@gmail.com>
2021-05-19 15:40:13 +0200

    Portfolio Radio Buttons

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -1,6 +1,17 @@
 import React, {Component} from 'react';
 
 class Project extends Component {
+
+    state = {
+        showInfo: false
+    }
+
+    handleInfo = () => {
+        this.setState({
+            showInfo: !this.state.showInfo
+        })
+    }
+
     render() {
         let {name, languagesIcons, source, info, picture} = this.props.item;
 
@@ -12,10 +23,31 @@ class Project extends Component {
                     )}
                 </div>
                 <h3>{name}</h3>
-                <img src={picture} alt=""/>
-                <span className="infos">
+                <img src={picture} alt="" onClick={this.handleInfo}/>
+                <span className="infos" onClick={this.handleInfo}>
                     <i className="fas fa-plus-circle"></i>
                 </span>
+
+                {
+                    this.state.showInfo && (
+                        <div className="showInfos">
+                            <div className="infosContent">
+                                <div className="head">
+                                    <h2>{name}</h2>
+                                    <div className="souceCode">
+                                        <a href={source} rel="noopener noreferrer" className="button" target="_blank">Code Source</a>
+                                    </div>
+                                </div>
+                                <p className="text">{info}</p>
+
+                                <div className="button return" onClick={this.handleInfo}>
+                                    Retourner sur la page
+                                </div>
+
+                            </div>
+                        </div>
+                    )
+                }
             </div>
         );
     }
